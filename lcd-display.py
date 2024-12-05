e976699c2968bf716005be6d11359e8a99713ffa
yujin08300 <yujin0830laura@naver.com>
2024-12-05 12:01:35 +0900

    Update lcd-display.py

--- lcd-display.py
+++ lcd-display.py
@@ -146,7 +146,7 @@ def detect_features(image):
 if __name__ == "__main__":
     try:
         lcd_init()
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(1)
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
